b66806b19f511eac154e3b53e78265426e905f61
unknown <npbarsha31@gmail.com>
2024-09-08 17:24:38 +0530

    New Changes  Related to King's Royal Riders

---
+++ src/component/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Riders.css";
+
+function NavBar() {
+    return (
+        <div className="nav-container">
+            <div className="navbar">
+                <h2>King's Royal Riders</h2>
+                <h2 className="nav">FAQ</h2>
+                <h2 className="nav">GALLERY</h2>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
